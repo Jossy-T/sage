457e50bc7512a5995f0cec5c44a36e95f67b6d02
jossy <yoseftanu994@gmail.com>
2025-07-09 03:52:36 -0700

    c++ data types

---
+++ c++/Data Types.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int age;
+	age=21;
+	string name= "Jossy";
+	
+	cout<<"my age is:"<<age<<endl;
+	cout<<"my name is-"<<name<<endl;
+
+	return 1;
+}
