bba14c124f718a2a5dd4e6315cc9cd3dbba1ba6a
jossy <yoseftanu994@gmail.com>
2025-07-22 04:04:55 -0700

    form

---
+++ sage html css js/web2.js
@@ -0,0 +1,42 @@
+function greet(){
+    alert('hello')
+}
+
+function sum(){
+    let x=3;
+    let y=8;
+    y=5;
+    alert(x+y);
+}
+
+let person={name:"Jossy", Fathername:"Tanu", age:"20" }
+console.log(person.name)
+
+let array=["A","r"]
+console.log(array[1]) 
+
+const js= true;
+console.log("boolean: " ,js);
+
+sum();
+
+let age=25;
+if(age>=25){
+    console.log("you are an adult");
+}
+
+// else if(age>=12 || =<18){
+//     console.log("You are chiled");
+// }
+else{
+    console.log("you are minor");
+}
+
+for(let i=1;i<8;i++){
+    console.log(i);
+}
+
+
+
+
+
