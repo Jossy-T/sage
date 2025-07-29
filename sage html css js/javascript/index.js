557e7325bb9b34f908e02eb4977378161a2a02f2
jossy <yoseftanu994@gmail.com>
2025-07-29 03:57:35 -0700

    javascript ideas

--- sage html css js/javascript/index.js
+++ sage html css js/javascript/index.js
@@ -19,3 +19,42 @@ function toggle(){
         bulb="on"
     }
 }
+
+const person={
+    firstName:"Yosef",
+    lastName:"Tanu",
+    age:21,
+    adress:{
+        country:"Ethiopia",
+        city:"A.A",
+        subCity:"Lemikura",
+        woreda:"13",
+    },
+
+    fullName: function(){
+        return this.firstName+" "+this.lastName
+    }
+
+}
+
+array=[
+    {name:"jossy", lastName:".T", age:23,},
+    {carname:"Ford Mustang", model:"560", color:"mat black"},
+]
+
+console.log(array[1].carname);
+
+console.log(person.adress.subCity);
+console.log(person.fullName());
+
+const date= new Date();
+const update= new Date(2025,14,32);
+console.log(update);
+
+// conditional statement in short hand nullish in short hand.
+let name;
+(45>5)?value=true:value=false
+
+console.log("the statement is"+" "+value);
+
+console.log(name??"73772828");
