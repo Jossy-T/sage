c74b0ba3b77db6e677553cd22d4524635d34403b
jossy <yoseftanu994@gmail.com>
2025-07-31 04:09:30 -0700

    react

---
+++ sage html css js/react/react-class/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./component/Button"
+import "./App.css"
+function App(){
+  return <div>
+    <h1>First component</h1>
+    <Button name="Buy Now"/>
+    <br/><br/>
+    <Button name="order"/>
+  </div>
+}
+
+export default App
---
+++ sage html css js/react/react-class/src/component/Button.jsx
@@ -0,0 +1,5 @@
+function Button(props){
+    return<button className="btn1">{props.name}</button>
+}
+
+export default Button;
