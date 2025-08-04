8d0e09ed8e8112b0e9cc7f83a3a6eeb60d7cc74e
jossy <yoseftanu994@gmail.com>
2025-08-04 04:32:21 -0700

    test

---
+++ userinfo.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+using namespace std;
+int main(){
+	string name, school, favoritesubject;
+	int age;
+	cout<<"Enter your name"<<endl;
+	cin>> name;
+	
+	cout<<"Enter your age"<<endl;
+	cin>> age;
+	
+	cout<<"Enter your school"<<endl;
+	cin>> school;
+	
+	cout<<"Enter your favorite subject"<<endl;
+	cin>> favoritesubject;
+	cout<<endl;
+	
+	cout<<"name: "<<name<<endl;
+	cout<<"age: "<<age<<endl;
+	cout<<"school: "<<school<<endl;
+	cout<<"favoritesubject: "<<favoritesubject<<endl;
+	
+	return 0;
+}
