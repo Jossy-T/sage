fa208d37bf69d32186d4fc49ca8c658dcdae0cff
jossy <yoseftanu994@gmail.com>
2025-08-11 04:05:48 -0700

    task

---
+++ react/mytask/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import './App.css';
+import Layout from './route/Layout';
+import About from './route/pages/About';
+import Home from './route/pages/Home';
+import Task from './route/pages/Task';
+
+function App() {
+  return (
+   <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route index element={<About/>}/>
+           <Route index element={<Task/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+   </div>
+  );
+}
+
+export default App;
---
+++ react/mytask/src/route/NavBar.jsx
@@ -0,0 +1,9 @@
+import {Link} from "react-router-dom"
+
+export default function NavBar(){
+    return<>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/add"> Add Task</Link>
+    </>
+}
